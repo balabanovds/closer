f3e557af371f428d9b44938a1833075cee999a11
Balabanov Denis <denis.balabanov@finhood.teams>
2021-11-09 12:08:37 +0300

    add comments

--- options.go
+++ options.go
@@ -5,20 +5,23 @@ import (
 	"os"
 )
 
+//WithClosers .
 func WithClosers(closers ...io.Closer) Option {
 	return func(c *Closer) {
 		c.closers = append(c.closers, closers...)
 	}
 }
 
+//WithCloseFuncs .
 func WithCloseFuncs(funcs ...func()) Option {
 	return func(c *Closer) {
 		c.closeFuncs = append(c.closeFuncs, funcs...)
 	}
 }
 
+//WithSignals add signals to defaults SIGINT, SIGTERM
 func WithSignals(sigs ...os.Signal) Option {
 	return func(c *Closer) {
-
+		c.sigs = append(c.sigs, sigs...)
 	}
 }
