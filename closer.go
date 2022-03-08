2514c114a91f0c6ba5c008f1ebec52fad0c610cc
Denis Balabanov <balabanov.ds@gmail.com>
2022-03-08 21:10:56 +0300

    add wait to closer

--- closer.go
+++ closer.go
@@ -1,37 +1,37 @@
 package closer
 
 import (
-	"fmt"
+	"context"
 	"io"
+	"log"
 	"os"
 	"os/signal"
 	"sync"
 	"syscall"
 	"time"
-
-	"go.uber.org/zap"
 )
 
-//Closer contains all service closers to handle 'em gently on signal received from OS
+// Closer contains all service closers to handle 'em gently on signal received from OS
 type Closer struct {
 	sync.Mutex
+	once       sync.Once
 	closers    []io.Closer
 	closeFuncs []func()
 	sigCh      chan os.Signal
 	sigs       []os.Signal
 	timeout    time.Duration
-	log        *zap.Logger
+	done       chan struct{}
 }
 
 type Option func(*Closer)
 
-//New ...
-func New(log *zap.Logger, timeout time.Duration, opts ...Option) *Closer {
+// New ...
+func New(timeout time.Duration, opts ...Option) *Closer {
 	c := Closer{
 		sigs:    []os.Signal{syscall.SIGINT, syscall.SIGTERM},
 		sigCh:   make(chan os.Signal, 1),
 		timeout: timeout,
-		log:     log,
+		done:    make(chan struct{}),
 	}
 
 	for _, opt := range opts {
@@ -42,51 +42,72 @@ func New(log *zap.Logger, timeout time.Duration, opts ...Option) *Closer {
 		signal.Notify(c.sigCh, c.sigs...)
 
 		sig := <-c.sigCh
-		c.log.Info("received syscall signal", zap.String("sig", sig.String()))
+		log.Printf("received syscall signal: %s", sig.String())
 		c.drop()
 	}()
 
 	return &c
 }
 
-//AddCloser any io.Closer
+// AddCloser any io.Closer
 func (c *Closer) AddCloser(cl io.Closer) {
 	c.Lock()
 	defer c.Unlock()
 	c.closers = append(c.closers, cl)
 }
 
-//AddFunc any func() that will be run on exit
+// AddFunc any func() that will be run on exit
 func (c *Closer) AddFunc(f func()) {
 	c.Lock()
 	defer c.Unlock()
 	c.closeFuncs = append(c.closeFuncs, f)
 }
 
-//Close force close all underlying Closers
+// Close force close all underlying Closers
 func (c *Closer) Close() {
 	c.drop()
 }
 
+func (c *Closer) Wait() {
+	<-c.done
+}
+
 func (c *Closer) drop() {
-	c.Lock()
-	defer c.Unlock()
+	c.once.Do(func() {
+		c.Lock()
+		defer c.Unlock()
 
-	for _, cl := range c.closers {
-		go func(cl io.Closer) {
-			err := cl.Close()
-			if err != nil {
-				c.log.Error("failed to close", zap.Error(err))
-			}
-		}(cl)
-	}
+		var wg sync.WaitGroup
 
-	for _, cf := range c.closeFuncs {
-		go cf()
-	}
+		ctx, cancel := context.WithTimeout(context.Background(), c.timeout)
 
-	c.log.Info(fmt.Sprintf("waiting %s before terminate", c.timeout.String()))
-	time.Sleep(c.timeout)
+		go func() {
+			wg.Wait()
+			cancel()
+		}()
 
-	os.Exit(1)
+		for _, cl := range c.closers {
+			wg.Add(1)
+			go func(cl io.Closer) {
+				defer wg.Done()
+				err := cl.Close()
+				if err != nil {
+					log.Printf("failed to close: %s", err)
+				}
+			}(cl)
+		}
+
+		for _, cf := range c.closeFuncs {
+			wg.Add(1)
+			go func(f func()) {
+				defer wg.Done()
+				f()
+			}(cf)
+		}
+
+		log.Printf("waiting %s before terminate or end up earlier if funcs ready", c.timeout.String())
+		<-ctx.Done()
+
+		c.done <- struct{}{}
+	})
 }
--- options.go
+++ options.go
@@ -5,21 +5,21 @@ import (
 	"os"
 )
 
-//WithClosers .
+// WithClosers add any io.Closer to internal state
 func WithClosers(closers ...io.Closer) Option {
 	return func(c *Closer) {
 		c.closers = append(c.closers, closers...)
 	}
 }
 
-//WithCloseFuncs .
+// WithCloseFuncs add any func to internal state
 func WithCloseFuncs(funcs ...func()) Option {
 	return func(c *Closer) {
 		c.closeFuncs = append(c.closeFuncs, funcs...)
 	}
 }
 
-//WithSignals add signals to defaults SIGINT, SIGTERM
+// WithSignals add os signals to defaults SIGINT, SIGTERM
 func WithSignals(sigs ...os.Signal) Option {
 	return func(c *Closer) {
 		c.sigs = append(c.sigs, sigs...)
